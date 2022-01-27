bd8d5f8ddca243b6996dd5a56285298858b59b4c
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-01-27 12:46:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# 1-4 Exercise 1
+
+### The assignment
+Edit the file named "student.py". Create a program that adds “ing” to the end of a verb that’s input by the user.  
+#### Input  
+jump  
+#### Output  
+jumping  
+#### Note
+there shouldn't be a space between the verb and the "ing"
+
+
+## Check your work
+Select the "Actions" tab on GitHub and select your latest commit. This will show you if your exercise passes all test cases.
---
+++ student_test.py
@@ -0,0 +1,79 @@
+import pytest
+import student 
+
+
+
+def test_jump():
+    input_values=['jump']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert output[1]=='jumping'
+
+def test_eat():
+    input_values=['eat']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert output[1]=='eating'    
+
+def test_scarps():
+    input_values=['scarp']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert output[1]=='scarping'
+def test_groundbreak():
+    input_values=['groundbreak']
+    output=[]
+
+    def mock_input(s=None):
+        if s is not None:
+            output.append(s)
+            return input_values.pop(0)
+        else:
+            output.append("")
+            return input_values.pop(0)
+    
+    student.input = mock_input
+    student.print = lambda s : output.append(s)
+
+    student.main()
+
+    assert output[1]=='groundbreaking'
