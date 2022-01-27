fc80969d9e5949ba19c4ee72e9179bac61ba6ae4
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-01-27 12:56:40 -0500

    Update student_test.py

--- student_test.py
+++ student_test.py
@@ -3,8 +3,8 @@ import student
 
 
 
-def test_jump():
-    input_values=['jump']
+def test_default():
+    input_values=['Power Glove', '1', '299.99']
     output=[]
 
     def mock_input(s=None):
@@ -20,10 +20,10 @@ def test_jump():
 
     student.main()
 
-    assert output[1]=='jumping'
-
-def test_eat():
-    input_values=['eat']
+    assert output[3].lower()=='power glove has 1 in stock, and it costs $299.99', "It must be exact"
+ 
+def test_two():
+    input_values=['blarg', '17', '1.99']
     output=[]
 
     def mock_input(s=None):
@@ -39,41 +39,5 @@ def test_eat():
 
     student.main()
 
-    assert output[1]=='eating'    
+    assert output[3].lower()=='blarg has 17 in stock, and it costs $1.99', "It must be exact"
 
-def test_scarps():
-    input_values=['scarp']
-    output=[]
-
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
-
-    student.main()
-
-    assert output[1]=='scarping'
-def test_groundbreak():
-    input_values=['groundbreak']
-    output=[]
-
-    def mock_input(s=None):
-        if s is not None:
-            output.append(s)
-            return input_values.pop(0)
-        else:
-            output.append("")
-            return input_values.pop(0)
-    
-    student.input = mock_input
-    student.print = lambda s : output.append(s)
-
-    student.main()
-
-    assert output[1]=='groundbreaking'
